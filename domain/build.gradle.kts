574169304c4da0c56947a8ef18e20b499651eb3d
한성민 <seongmin.han@cjconnect.io>
2024-02-28 13:23:38 +0900

    버전 카테고리 정리 주석

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17 //https://techblog.gccompany.co.kr/%EC%9A%B0%EB%A6%AC%ED%8C%80%EC%9D%B4-jdk-17%EC%9D%84-%EB%8F%84%EC%9E%85%ED%95%9C-%EC%9D%B4%EC%9C%A0-ced2b754cd7
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
